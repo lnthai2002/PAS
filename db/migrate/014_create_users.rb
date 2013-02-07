465e98a6bae2f8ea0c3b4756adaddbb697c046d2
nhutthaile <nhutthaile@mentel.com>
2013-02-07 17:30:25 -0500

    Issue_31: migration for User

--- db/migrate/014_create_users.rb
+++ db/migrate/014_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.column :email, :string
+      t.column :encrypted_password, :string
+      t.column :encryption_salt, :string
       t.timestamps
     end
   end
