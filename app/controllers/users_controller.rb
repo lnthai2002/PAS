4c4ba34e92909a69b3e83daa9f59f1ffd1124eae
Thai Le <nhut_le.applewoodconsulting@gmail.com>
2013-02-07 23:02:01 -0500

    Issue_31: controller and view for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  before_filter :fetch_user, :only=>[:show, :edit, :update, :destroy]
+  set_tab :account
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to users_path
+    else
+      render :save
+    end
+  end
 end
