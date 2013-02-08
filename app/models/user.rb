168b9439f1a2be8f95f9fd6f83aa16f87e9265ce
Thai Le <nhut_le.applewoodconsulting@gmail.com>
2013-02-07 23:00:44 -0500

    Issue_31: user spec and implementation support encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
   # attr_accessible :title, :body
+  before_validation(:on=>:create) {set_salt}
+  before_save :encrypt_password
+
+protected
+
+  def set_salt
+    self.encryption_salt = Time.now.to_i
+  end
+
+  #Desc: only encrypt password if it has not been set (new) or user explicitly enter password update
+  def encrypt_password
+    if self.new_record? || !self.password.blank?
+      self.encrypted_password = Digest::SHA256.hexdigest("#{self.encryption_salt}::#{@password}")
+    end
+  end
 end
