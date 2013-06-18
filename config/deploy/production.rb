a67c897b669adf01d6ef9a256cd0f01c5f9c9790
Thai Le <lnthai2002@gmail.com>
2013-06-17 20:43:15 -0400

    issue_46: Simplify deployment script to ease maintenent when capistrano update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,14 @@
 
-set :branch,     "origin/production"
+set :branch,     "production"
 set :rails_env, 'production'
 
 #declare production servers
 default_environment["RAILS_ENV"] = 'production'
-role :web, "darkportal.no-ip.info"                          # Your HTTP server, Apache/etc
-role :app, "darkportal.no-ip.info"                          # This may be the same as your `Web` server
-role :db,  "darkportal.no-ip.info", :primary => true # This is where Rails migrations will run
+role :web, "darkportal.no-ip.info"                      #Your HTTP server, Apache/etc
+role :app, "darkportal.no-ip.info"                      #This may be the same as your `Web` server
+role :db,  "darkportal.no-ip.info", :primary => true    #This is where Rails migrations will run
 set :deploy_to, "/var/www/PAS"
-set :user, "nhut_le" #user on production server to deploy
+set :user, "nhut_le"                                    #user on production server to deploy
 set :use_sudo, false
 
 set :db_config_loc, "/home/CONFIG/ubuntu/var/www/PAS/config/#{rails_env}.database.yml"
