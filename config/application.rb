1766c4a7065b58d85819ee9d251c154e4783ee38
Thai Le <lnthai2002@gmail.com>
2013-11-05 20:21:40 +0000

    issue_124: use CDN to deliver jquery in production mode

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,7 @@ module Pas
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['jquery.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
