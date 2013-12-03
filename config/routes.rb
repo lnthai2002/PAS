d0505670fe4198d78c5d6f0afb1bf01a13bce452
Thai Le <lnthai2002@gmail.com>
2013-12-02 22:24:16 -0500

    issue_139: change to fit rails4 bundle group and routing

--- config/routes.rb
+++ config/routes.rb
@@ -63,5 +63,5 @@ Pas::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  get ':controller(/:action(/:id(.:format)))'
 end
