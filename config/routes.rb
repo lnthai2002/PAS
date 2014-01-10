1671b5c5ef88a1227e8fc90cdf523ac5e3da1530
Thai Le <lnthai2002@gmail.com>
2014-01-10 15:16:06 -0500

    issue_144: cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
+# See how all your routes lay out with "rake routes"
 Pas::Application.routes.draw do
-
   #engines
   mount Cooking::Engine => "/cooking"
   mount Financial::Engine => "/financial"  
 
+  resources :groups
+  resources :users
   resources :songs
 #  match 'songs/download/:id'=>'songs#download', :id => /[^\/]+/
-  resources :users
-  resources :groups
 
   root :to => 'songs#index'
-  # See how all your routes lay out with "rake routes"
 
   delete 'logout', to: 'sessions#logout'
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  get ':controller(/:action(/:id(.:format)))'
 end
