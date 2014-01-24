9b01b77b38fcc786e75b006a536bee5000f3ad00
Thai Le <lnthai2002@gmail.com>
2014-01-23 19:09:06 -0500

    issue_155: remove user manager usecase from main app

--- app/controllers/sessions_controller.rb
+++
@@ -1,6 +0,0 @@
-class SessionsController < ApplicationController
-  def logout
-    reset_session
-    CASClient::Frameworks::Rails::Filter.logout(self)
-  end
-end
--- app/models/group.rb
+++
@@ -1,6 +0,0 @@
-class Group < ActiveRecord::Base
-  #attr_accessible :name
-  has_many :memberships
-  has_many :users, :through=>:memberships
-  validates :name, :presence=>true
-end
